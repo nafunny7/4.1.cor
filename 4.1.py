3cc1e2d2dc1ef26919fed5e0b0f3dd4357b4959e
nafunny7 <denisstradymov35932@gmail.com>
2024-08-27 16:09:49 +0200

    Add files via upload

---
+++ 4.1.py
@@ -0,0 +1,9 @@
+from true_math import divide as tm
+from fake_math import divide as fm
+
+print(tm(9, 54))
+print(tm(999, 3))
+print(tm(52, 0))
+print(fm(332, 8))
+print(fm(23942873489712890346, 0))
+print(fm(1000, 25))
